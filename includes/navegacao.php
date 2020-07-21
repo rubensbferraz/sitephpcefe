91183899b55a57f7f3ce6ffc37e24ad5509aeb26
Carlos Rubens B Ferraz <rubensbferraz@gmail.com>
2020-07-21 18:22:23 -0300

    Atualização recurso de navegação 2

--- includes/navegacao.php
+++ includes/navegacao.php
@@ -1,5 +1,5 @@
-<?php  $base       = 'sitephpcefe'; ?>
-<nav class="navbar navbar-light bg-light row d-inline-block ml-1 p-2 menuCadastro";>
+<?php  $base = 'sitephpcefe'; ?>
+<nav class="navbar navbar-light bg-light row d-inline-block ml-1 p-2";>
   <a class="btn btn-light text-center p-2" href="<?php $conn ?>/<?php echo $base ?>/">Home
     <img src= "<?php $conn ; ?>/<?php echo $base ;?>/imagens/navegacaoDireita.png" width="25"  alt="">
   </a>
